d758ca7f05d6a53ea1d6a00435388143c408ac3e
bufander <bufanderbufander@gmail.com>
2022-09-22 13:01:41 +0200

    feat: separated strategy into BaseStrategy

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -3,88 +3,31 @@
 pragma solidity 0.8.14;
 pragma experimental ABIEncoderV2;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
+
+import {IERC20, BaseStrategy} from "BaseStrategy.sol";
 
 import "./interfaces/ILendingPool.sol";
 import "./interfaces/ILendingPoolAddressesProvider.sol";
 import "./interfaces/IProtocolDataProvider.sol";
-import "./interfaces/IVault.sol";
 
-contract Strategy {
-    using Math for uint256;
-
+contract Strategy is BaseStrategy {
     IProtocolDataProvider public constant protocolDataProvider =
         IProtocolDataProvider(0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d);
 
-    string public name;
     address public immutable aToken;
-    address public immutable asset;
-    address public vault;
 
-    constructor(address _vault, string memory _name) {
-        vault = _vault;
-        name = _name;
-        asset = IVault(vault).asset();
+    constructor(address _vault, string memory _name)
+        BaseStrategy(_vault, _name)
+    {
         (address _aToken, , ) = protocolDataProvider.getReserveTokensAddresses(
             asset
         );
         aToken = _aToken;
     }
 
-    function maxDeposit(address receiver)
-        public
-        view
-        returns (uint256 maxAssets)
-    {
-        maxAssets = type(uint256).max;
-    }
-
-    function convertToAssets(uint256 shares) public view returns (uint256) {
-        // 1:1
-        return shares;
-    }
-
-    function convertToShares(uint256 assets) public view returns (uint256) {
-        // 1:1
-        return assets;
-    }
-
-    function totalAssets() public view returns (uint256) {
-        return _totalAssets();
-    }
-
-    function balanceOf(address owner) public view returns (uint256) {
-        if (owner == vault) {
-            return _totalAssets();
-        }
-        return 0;
-    }
-
-    function deposit(uint256 assets, address receiver)
-        public
-        returns (uint256)
-    {
-        require(msg.sender == vault && msg.sender == receiver);
-
-        // transfer and invest
-        IERC20(asset).transferFrom(vault, address(this), assets);
-        _invest();
-        return assets;
-    }
-
-    function maxWithdraw(address owner) public view returns (uint256) {
-        // TODO: review
+    function maxWithdraw(address owner) public view override returns (uint256) {
         return Math.min(IERC20(asset).balanceOf(aToken), _totalAssets());
-    }
-
-    function withdraw(
-        uint256 amount,
-        address receiver,
-        address owner
-    ) public returns (uint256) {
-        require(amount <= maxWithdraw(owner), "withdraw more than max");
-        return _withdraw(amount, receiver, owner);
     }
 
     function _freeFunds(uint256 _amount)
@@ -112,17 +55,17 @@ contract Strategy {
         uint256 amount,
         address receiver,
         address owner
-    ) internal returns (uint256) {
+    ) internal override returns (uint256) {
         uint256 amount_to_withdraw = _freeFunds(amount);
         IERC20(asset).transfer(receiver, amount_to_withdraw);
         return amount_to_withdraw;
     }
 
-    function _totalAssets() internal view returns (uint256) {
+    function _totalAssets() internal view override returns (uint256) {
         return balanceOfAsset() + balanceOfAToken();
     }
 
-    function _invest() internal {
+    function _invest() internal override {
         uint256 available_to_invest = balanceOfAsset();
         require(available_to_invest > 0, "no funds to invest");
         _depositToAave(available_to_invest);
